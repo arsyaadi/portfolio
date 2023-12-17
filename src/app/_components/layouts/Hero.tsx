31ec6e0a94833b5f9d9879996b3a8b900f826e30
arsya adi <arsyaadi@gmail.com>
2023-12-18 00:52:59 +0700

    [FIX]: fix build

--- src/app/_components/layouts/Hero.tsx
+++ src/app/_components/layouts/Hero.tsx
@@ -13,13 +13,13 @@ const Hero = () => {
 
   return (
     <div className="text-start w-full flex flex-col gap-y-4 pt-80 pb-56" id="#">
-      <h4 className="font-SF-mono text-aquamarine">Hi I'am</h4>
+      <h4 className="font-SF-mono text-aquamarine">Hi I&apos;am</h4>
       <h1 className={`${oswald.className} text-7xl text-titan-white`}>
         Arsya Adi Setiawan Putra
       </h1>
       <BackgroundAnimation />
       <p className="max-w-xl text-lg text-regent-gray font-Calibre">
-        I'm a software engineer based in Yogyakarta, specializing in
+        I&apos;m a software engineer based in Yogyakarta, specializing in
         building (and occasionally designing) exceptional websites,
         applications, and everything in between.
       </p>
