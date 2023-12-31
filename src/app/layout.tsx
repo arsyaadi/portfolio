812962e88959342b0a48a14fd93ddf5f49b2fd46
arsya adi <arsyaadi@gmail.com>
2023-12-31 17:59:49 +0700

    perf(seo): improve the description and keyword of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Arsya Adi Setiawan',
-  description: 'A Frontend Web Developer who has a high enthusiasm for web application development. Has main capabilities such as Javascript, Typescript, React.JS, Vue.Js, HTML, and CSS  Interested in exploring new things to increase knowledge, have good adaptability, and are able to work professionally both as a team and individually.',
-  keywords: 'Arsya Adi Setiawan, Arsya, Adi, Setiawan, Frontend, Web, Developer, Frontend Web Developer, Web Developer, Frontend Developer, Frontend Web Developer in Indonesia, Frontend Web Developer',
+  title: "Arsya Adi Setiawan",
+  description: "Hi! I'm Arsya Adi Setiawan Putra, a Frontend Web Developer passionate about creating dynamic web applications. Proficient in Javascript, Typescript, React.JS, Vue.JS, HTML, and CSS. I thrive on exploring new technologies to expand my knowledge and bring professionalism to both team and individual projects.",
+  keywords: ["Arsya Adi Setiawan", "Arsya", "Adi", "Setiawan", "Frontend", "Web", "Developer", "Frontend Web Developer", "Web Developer", "Frontend Developer", "Frontend Web Developer in Indonesia", "Frontend Web Developer"],
   icons: [{ rel: 'icon', href: Favicon.src, url: Favicon.src}],
 }
 
